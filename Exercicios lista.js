55535c705dafd71e2cdad152b256f65b3cc93c4c
Patrick Rodrigues <PatrickRodriguesTECH@outlook.com>
2024-08-01 18:38:07 -0300

    Add files via upload

---
+++ Exercicios lista.js
@@ -0,0 +1,21 @@
+//Crie uma lista vazia, com o nome listaGenerica.
+let listaGenerica = [];
+
+//Crie uma lista de linguagens de programação chamada linguagensDeProgramacao com os seguintes elementos: 'JavaScript','C','C++', 'Kotlin' e 'Python'.
+let linguagemDeProgramacao = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+
+//Adicione à lista linguagensDeProgramacao os seguintes elementos: 'Java', 'Ruby' e 'GoLang'.
+linguagemDeProgramacao.push('Java', 'Ruby', 'GoLang');
+console.log(linguagemDeProgramacao);
+
+
+//Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.
+let financeiro = ['Patrick', 'Marcos', 'Danilo'];
+console.log(financeiro[0]);
+
+//Crie uma lista com 3 nomes e exiba no console apenas o segundo elemento.
+let quarto = ['Winn', 'Bel', 'Tadeu'];
+console.log(quarto[1]);
+
+//Crie uma lista com 3 nomes e exiba no console apenas o último elemento.
+let carro = ['306', 'Accord', 'Vectra GLS'];
